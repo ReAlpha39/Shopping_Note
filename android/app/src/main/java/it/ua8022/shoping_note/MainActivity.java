d6996d7303436d348d72ac874dc228fdf6180c5e
YunusNS <yunusw718@gmail.com>
2019-09-19 12:56:43 +0700

    Initial commit

---
+++ android/app/src/main/java/it/ua8022/shoping_note/MainActivity.java
@@ -0,0 +1,13 @@
+package it.ua8022.shoping_note;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
