ed5ff33c89f106242203894920b7fcf61b3f9fb5
YunusNS <yunusw718@gmail.com>
2019-09-20 21:32:40 +0700

    add class model

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shoping_note/view/formPage.dart';
 
@@ -27,4 +28,19 @@ class _HomeState extends State<Home> {
   void navigateToFormPage(){
     Navigator.push(context, MaterialPageRoute(builder: (context) => FormPage()));
   }
+}
+
+class Record {
+  final int jumlahDoc;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      :  assert(map['jumlahDoc'] != null),
+        jumlahDoc = map['jumlahDoc'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$jumlahDoc>";
 }
