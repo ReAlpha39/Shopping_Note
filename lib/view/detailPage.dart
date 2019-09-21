2b1c7348702d4f6bcf1de4c42a38b26116b7d62e
YunusNS <yunusw718@gmail.com>
2019-09-21 09:43:25 +0700

    add data model untuk DetailPage

--- lib/view/detailPage.dart
+++ lib/view/detailPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
@@ -15,4 +16,25 @@ class _DetailPageState extends State<DetailPage> {
       body: Container(),
     );
   }
+}
+
+class Item {
+  final String nama;
+  final String deskripsi;
+  final int harga;
+  final DocumentReference reference;
+
+  Item.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['Nama'] != null),
+        assert(map['Deskripsi'] != null),
+        assert(map['Harga'] != null),
+        nama = map['Nama'],
+        deskripsi = map['Deskripsi'],
+        harga = map['Harga'];
+
+  Item.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Item<$nama:$deskripsi:$harga>";
 }
