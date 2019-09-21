4e27ca7431af780c6eb9d8749c832e97dd9c653f
YunusNS <yunusw718@gmail.com>
2019-09-21 10:57:22 +0700

    Add SingleChildScrollView agar dapat bisa di scroll

--- lib/view/formPage.dart
+++ lib/view/formPage.dart
@@ -27,91 +27,93 @@ class _FormPageState extends State<FormPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Form(
-          key: _formKey,
-          child: Column(children: <Widget>[
-            DateTimeField(
-              decoration: InputDecoration(
-                labelText: 'Tanggal'
-              ),
-              readOnly: true,
-              format: dateFormat,
-              initialValue: DateTime.now(),
-              onShowPicker: (context, currentValue) {
-                return showDatePicker(
-                  context: context,
-                  firstDate: DateTime(1900),
-                  initialDate: currentValue ?? DateTime.now(),
-                  lastDate: DateTime(2100)
-                );
-              },
-              onSaved: (value) {_tanggal = dateFormat.format(value);},
-            ),
-            TextFormField(
-              controller: namaC,
-              decoration: InputDecoration(
-                labelText: 'Nama'
-              ),
-              validator: (i) {
-                if (i == ''){
-                  return 'Nama harus diisi';
-                }else{
-                  return null;
-                }
-              },
-              onSaved: (value) {_nama = value;},
-            ),
-            TextFormField(
-              maxLines: 3,
-              controller: deskC,
-              decoration: InputDecoration(
-                labelText: 'Deskripsi'
-              ),
-              onSaved: (value) {_deskripsi = value;},
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Total Belanja'
-              ),
-              onSaved: (value) {_harga = int.parse(value);},
-              validator: (i) {
-                if (i == '') {
-                  return 'Total Belanja harus diisi';
-                }
-                if (int.tryParse(i) == null) {
-                  return 'Total Belanja harus berupa angka';
-                } else {
-                  return null;
-                }
-              },
-              controller: hargaC,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                RaisedButton(
-                  child: Text('Cancel'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+        child: SingleChildScrollView(
+            child: Form(
+            key: _formKey,
+            child: Column(children: <Widget>[
+              DateTimeField(
+                decoration: InputDecoration(
+                  labelText: 'Tanggal'
                 ),
-                RaisedButton(
-                  child: Text('Save'),
-                  onPressed: () {
-                    var form = _formKey.currentState;
-                    if (form.validate()) {
-                      form.save();
-                      updateDoc(_tanggal, _nama, _deskripsi, _harga);
+                readOnly: true,
+                format: dateFormat,
+                initialValue: DateTime.now(),
+                onShowPicker: (context, currentValue) {
+                  return showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900),
+                    initialDate: currentValue ?? DateTime.now(),
+                    lastDate: DateTime(2100)
+                  );
+                },
+                onSaved: (value) {_tanggal = dateFormat.format(value);},
+              ),
+              TextFormField(
+                controller: namaC,
+                decoration: InputDecoration(
+                  labelText: 'Nama'
+                ),
+                validator: (i) {
+                  if (i == ''){
+                    return 'Nama harus diisi';
+                  }else{
+                    return null;
+                  }
+                },
+                onSaved: (value) {_nama = value;},
+              ),
+              TextFormField(
+                maxLines: 3,
+                controller: deskC,
+                decoration: InputDecoration(
+                  labelText: 'Deskripsi'
+                ),
+                onSaved: (value) {_deskripsi = value;},
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Total Belanja'
+                ),
+                onSaved: (value) {_harga = int.parse(value);},
+                validator: (i) {
+                  if (i == '') {
+                    return 'Total Belanja harus diisi';
+                  }
+                  if (int.tryParse(i) == null) {
+                    return 'Total Belanja harus berupa angka';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: hargaC,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                  RaisedButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
                       Navigator.pop(context);
-                    }
-                  },
-                )
-              ],),
-            )
-          ]),
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Save'),
+                    onPressed: () {
+                      var form = _formKey.currentState;
+                      if (form.validate()) {
+                        form.save();
+                        updateDoc(_tanggal, _nama, _deskripsi, _harga);
+                        Navigator.pop(context);
+                      }
+                    },
+                  )
+                ],),
+              )
+            ]),
+          ),
         ),
       ),
       
